8ba8009c2bcd1cd561ed8606d6b84b1d75b59523
Jaegar Sarauer <jaegars@hotmail.com>
2022-09-06 00:11:21 -0700

    Absolute path support for project + tests.

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,16 @@
-import type {Config} from 'jest';
+import path from 'path'
+import type { Config } from 'jest'
 
 export default async (): Promise<Config> => {
-  return {
-    verbose: true,
-    preset: 'ts-jest',
-    transform: {
-        '^.+\\.(ts|tsx)?$': 'ts-jest',
-    },
-  };
-};
+    return {
+        verbose: true,
+        preset: 'ts-jest',
+        transform: {
+            '^.+\\.(ts|tsx)?$': 'ts-jest',
+        },
+        // moduleNameMapper: {
+        //     'src/(.*)': '<rootDir>/src/$1',
+        // },
+        moduleDirectories: ['node_modules', path.join(__dirname, 'src')],
+    }
+}
