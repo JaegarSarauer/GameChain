f2ceb092c7a19e2f4348d6b7bf43b2f5dd4c7729
Jaegar Sarauer <jaegars@hotmail.com>
2022-09-09 15:04:14 -0700

    Properly exporting as module.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import GameController from './game/GameController';
+import GameInterface from './game/GameInterface';
+import GameProxy from './game/GameProxy';
+import Wallet from './objects/Wallet';
+import ReceiptStructure from './objects/receipt/ReceiptStructure';
+
+export { GameController, GameProxy, GameInterface, Wallet, ReceiptStructure };
